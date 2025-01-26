0f98c0f9a255d0d19336adfa79c16fd632349a1d
Kyllian LE BORGNE ROPERCH <me@hprivakos.net>
2025-01-26 11:08:05 +0100

    init

---
+++ src/index.ts
@@ -0,0 +1,31 @@
+import 'dotenv/config'
+import { Client, Events, GatewayIntentBits, Partials, TextChannel } from 'discord.js'
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessageReactions],
+  partials: [Partials.Message, Partials.Channel, Partials.Reaction]
+})
+async function main() {
+  const secret = { DISCORD_API_KEY: process.env.DISCORD_API_KEY! }
+
+  await client.login(secret.DISCORD_API_KEY)
+  const starredChannel = (await client.channels.fetch(process.env.STAR_CHANNEL!)) as TextChannel
+  const guild = await client.guilds.fetch(process.env.GUILD_ID!)
+
+  client.on(Events.MessageReactionAdd, async (reaction, user) => {
+    if (reaction.message.guildId !== guild.id) return
+    if (reaction.emoji.name == process.env.REACTION) {
+      await reaction.message.fetch(true)
+      await reaction.message.author?.fetch(true)
+      await starredChannel.send(
+        `<@${reaction.message.author?.id}> (starred by <@${user.id}>) said: ${reaction.message.content}`
+      )
+    }
+  })
+}
+
+client.once(Events.ClientReady, (c) => {
+  console.log(`Ready! Logged in as ${c.user.tag}`)
+})
+
+void main()
